49a3fb423ea4ebf1734230fabf69605445afaf46
thespbgamer <30112696+thespbgamer@users.noreply.github.com>
2025-02-17 16:30:08 +0000

    Compare Binary Trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,18 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+  // Structure check
+  if (a === null && b === null) {
+    return true;
+  }
 
+  // Structure check
+  if (a === null || b === null) {
+    return false;
+  }
+
+  // Value check
+  if (a.value !== b.value) {
+    return false;
+  }
+
+  return compare(a.left, b.left) && compare(a.right, b.right);
 }
