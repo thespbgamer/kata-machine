0d6243a66bc212f435473539849c06100b901a73
thespbgamer <30112696+thespbgamer@users.noreply.github.com>
2025-02-18 14:53:09 +0000

    DFS on BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(current: BinaryNode<number> | null, needle: number): boolean {
+  if (!current) {
+    return false;
+  }
 
+  if (current.value === needle) {
+    return true;
+  }
+
+  if (current.value < needle) {
+    return search(current.right, needle);
+  }
+
+  return search(current.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
 }
